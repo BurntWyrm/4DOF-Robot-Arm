047253fbd679395cc445410a490fbf7eb7755b8e
Burn't <burntwyrm@gmail.com>
2025-07-21 10:55:43 +1200

    First Commit

---
+++ main/robot_arm_code/robot_arm_code.ino
@@ -0,0 +1,20 @@
+// DOF Robot Controller Code
+
+// Pot Values
+int potBase = 2;
+int potShoulder = 4;
+int potElbow = 16;
+int potWrist = 17;
+
+// Button
+int end_effector = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
